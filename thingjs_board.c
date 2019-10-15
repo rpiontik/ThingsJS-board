9f70ddd5c912147de5c8a49c60d8ceeecb1d23e6
Пионтик Роман <rip@rabota.ru>
2019-10-15 14:31:10 +0300

    first commit

---
+++ thingjs_board.c
@@ -0,0 +1,45 @@
+/*
+ * hw_consts.c
+ *
+ *  Created on: 5 февр. 2019 г.
+ *      Author: rpiontik
+ */
+
+#include "thingjs_board.h"
+
+const char BOARD_ID[]		= "ESP32_CORE_BOARD";
+const char BOARD_VERSION[]	= "V2";
+
+const struct board_pin	BOARD_PINS_MAP[BOARD_TOTAL_NUMBER_OF_PINS] = {
+		{ .id = VIN,	.available = false,	.name = "VIN"},
+		{ .id = GND1,	.available = false,	.name = "GND"},
+		{ .id = GPIO13,	.available = true,	.name = "GPIO13"},
+		{ .id = GPIO12,	.available = true,	.name = "GPIO12"},
+		{ .id = GPIO14,	.available = true,	.name = "GPIO14"},
+		{ .id = GPIO27,	.available = true,	.name = "GPIO27"},
+		{ .id = GPIO26,	.available = true,	.name = "GPIO26"},
+		{ .id = GPIO25,	.available = true,	.name = "GPIO25"},
+		{ .id = GPIO33,	.available = true,	.name = "GPIO33"},
+		{ .id = GPIO32,	.available = true,	.name = "GPIO32"},
+		{ .id = GPIO35,	.available = true,	.name = "GPIO35"},
+		{ .id = GPIO34,	.available = true,	.name = "GPIO34"},
+		{ .id = GPIO39,	.available = true,	.name = "GPIO39"},
+		{ .id = GPIO36,	.available = true,	.name = "GPIO36"},
+		{ .id = EN,		.available = false,	.name = "EN"},
+
+		{ .id = GPIO23,	.available = true,	.name = "GPIO23"},
+		{ .id = GPIO22,	.available = true,	.name = "GPIO22"},
+		{ .id = GPIO1,	.available = true,	.name = "GPIO1"},
+		{ .id = GPIO3,	.available = true,	.name = "GPIO3"},
+		{ .id = GPIO21,	.available = true,	.name = "GPIO21"},
+		{ .id = GPIO19,	.available = true,	.name = "GPIO19"},
+		{ .id = GPIO18,	.available = true,	.name = "GPIO18"},
+		{ .id = GPIO5,	.available = true,	.name = "GPIO5"},
+		{ .id = GPIO17,	.available = true,	.name = "GPIO17"},
+		{ .id = GPIO16,	.available = true,	.name = "GPIO16"},
+		{ .id = GPIO4,	.available = true,	.name = "GPIO4"},
+		{ .id = GPIO2,	.available = true,	.name = "GPIO2"},
+		{ .id = GPIO15,	.available = true,	.name = "GPIO15"},
+		{ .id = GND2,	.available = false,	.name = "GND"},
+		{ .id = V3V3,	.available = false,	.name = "3V3"}
+};
---
+++ thingjs_board.h
@@ -0,0 +1,66 @@
+/*
+ * hw_consts.h
+ *
+ *  Created on: 5 февр. 2019 г.
+ *      Author: rpiontik
+ */
+
+#ifndef MAIN_LIBS_INTERFACES_I_CONSTS_H_
+#define MAIN_LIBS_INTERFACES_I_CONSTS_H_
+
+#include <stddef.h>
+#include <stdbool.h>
+
+//Hardware id
+extern const char BOARD_ID[];
+extern const char BOARD_VERSION[];
+
+//PINS
+//		name			id
+#define VIN				1004
+#define GND1			1003
+#define GPIO13			13
+#define GPIO12			12
+#define GPIO14			14
+#define GPIO27			27
+#define GPIO26			26
+#define GPIO25			25
+#define GPIO33			33
+#define GPIO32			32
+#define GPIO35			35
+#define GPIO34			34
+#define GPIO39			39
+#define GPIO36			36
+#define EN				1003
+
+#define V3V3			1002
+#define GND2			1001
+#define GPIO15			15
+#define GPIO2			2
+#define GPIO4			4
+#define GPIO16			16
+#define GPIO17			17
+#define GPIO5			5
+#define GPIO18			18
+#define GPIO19			19
+#define GPIO21			21
+#define GPIO3			3
+#define GPIO1			1
+#define GPIO22			22
+#define GPIO23			23
+
+//Number of pins that available for using
+#define BOARD_TOTAL_NUMBER_OF_PINS	30
+
+//Definition of pin on board
+struct board_pin {
+	int id;				//Internal Id of pin
+	bool available;		//True if pin is available to use
+	char * name;		//Human name of pin
+};
+
+
+//Hardware map of pins
+extern const struct board_pin BOARD_PINS_MAP[BOARD_TOTAL_NUMBER_OF_PINS];
+
+#endif /* MAIN_LIBS_INTERFACES_I_CONSTS_H_ */
